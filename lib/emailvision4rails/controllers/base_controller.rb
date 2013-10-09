72b93402e1ce0169d38bc0ba3d342ee6bba6d3df
kaluznyo <olivier@eboutic.ch>
2013-10-09 08:57:49 +0200

    More Debug

--- lib/emailvision4rails/controllers/base_controller.rb
+++ lib/emailvision4rails/controllers/base_controller.rb
@@ -4,7 +4,8 @@ require 'action_mailer/collector'
 
 module Emailvision4rails
 	class BaseController < ::AbstractController::Base
-		abstract!
+    warn "EmailVision4Rails::BaseController"
+    abstract!
 
 		include AbstractController::Logger
 		include AbstractController::Rendering
@@ -16,13 +17,14 @@ module Emailvision4rails
 
 		class << self
 			def respond_to?(method, include_private = false)
-				super || action_methods.include?(method.to_s)
+        warn "EmailVision4Rails::BaseController#respond_to"
+        super || action_methods.include?(method.to_s)
 			end
 
 			protected
 
 			def method_missing(method, *args)
-        puts "EmailVision4Rails::BaseController#method_missing"
+        warn "EmailVision4Rails::BaseController#method_missing"
 				return super unless respond_to?(method)
 				instance = new(method, *args)
 				instance.render_views
@@ -33,24 +35,28 @@ module Emailvision4rails
 		attr_internal :rendered_views
 
 	  def initialize(method_name=nil, *args)
-	    super()
+      warn "EmailVision4Rails::BaseController#initialize"
+      super()
 	    lookup_context.formats = [:text, :html] # Restrict rendering formats to html and text
 	    process(method_name, *args) if method_name
 	  end
 
 		def process(method_name, *args)
-			lookup_context.skip_default_locale!
+      warn "EmailVision4Rails::BaseController#process"
+      lookup_context.skip_default_locale!
 			super(method_name, *args)
 		end
 
 		def render_views
-			self.rendered_views = ::Emailvision4rails::RenderedViews.new(collect_responses(lookup_context.formats))
+      warn "EmailVision4Rails::BaseController#render_views"
+      self.rendered_views = ::Emailvision4rails::RenderedViews.new(collect_responses(lookup_context.formats))
 		end
 
 		private
 
 		def collect_responses(formats)
-			collector = ActionMailer::Collector.new(lookup_context) do 
+      warn "EmailVision4Rails::BaseController#collect_response"
+      collector = ActionMailer::Collector.new(lookup_context) do
 				render(action_name)
 			end			
 			responses = {}
